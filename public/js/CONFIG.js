e09e1e8e3c74f154e3335085bb169af80783acfa
mrdatdepzai <nguyenhuudat>
2021-10-09 14:54:13 +0700

    first commit

---
+++ public/js/CONFIG.js
@@ -0,0 +1,55 @@
+const __DATA__ = {
+    'config': {
+        'title': 'INFO|Nguyễn Hữu Đạt',
+        'favicon': 'home.png',
+    },
+    'cover-image': 'bia.jpg',
+    'avatar-image': 'avatar.jpg',
+    'name': 'Nguyễn Hữu Đạt',
+    'skills': [
+        'Developer',
+        'Designer',
+    ],
+    'bio-storys': [{
+        'image': 'admin.png',
+        'content': 'Giới thiệu',
+        'author': 'Chào Bạn 👋,tui sống tại TP Hà Nội.I am Student',
+    }, ],
+    'links': [{
+            'image': 'facebook.jpg',
+            'title': 'Facebook',
+            'show-value': '@mrdatdepzaii',
+            'value': 'https://www.facebook.com/MrDatDepZaii/',
+            'type': 0,
+        },
+        {
+            'image': 'zalo.jpg',
+            'title': 'Zalo',
+            'show-value': 'Nguyễn Hữu Đạt',
+            'value': 'https://zalo.me/mrdatdepzai',
+            'type': 0,
+        },
+
+        {
+            'image': 'gmail.jpg',
+            'title': 'Gmail',
+            'show-value': 'dat037sk@gmail.com',
+            'value': 'dat037sk@gmail.com',
+            'type': 1,
+        },
+        {
+            'image': 'phone.jpg',
+            'title': 'MB Bank',
+            'show-value': '0866051437',
+            'value': '0866051437',
+            'type': 1,
+        },
+        {
+            'image': 'momo.jpg',
+            'title': 'MoMo',
+            'show-value': '0866051437',
+            'value': '0866051437',
+            'type': 1,
+        }
+    ]
+};
